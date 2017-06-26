c62fa2be60c34d9b86332298bdb4a068cf482ca4
Mauro Tortonesi <mauro.tortonesi@unife.it>
2017-06-26 15:00:13 +0200

    Platform-agnostic reimplementation of GpdDistribution.

--- lib/erv/general_pareto_distribution.rb
+++ lib/erv/general_pareto_distribution.rb
@@ -1,42 +1,43 @@
-if RUBY_PLATFORM == 'java'
-  require 'java'
-  java_import org.apache.commons.math3.distribution.UniformRealDistribution
-end
-
 require 'erv/distribution'
 require 'erv/support/try'
 
 
 module ERV
 
+  # See https://en.wikipedia.org/wiki/Generalized_Pareto_distribution
   class GpdDistribution < Distribution
+    attr_accessor :mean, :variance
+
     def initialize(opts)
       super(opts)
 
       raise ArgumentError unless opts[:scale] and opts[:shape]
-      scale    = opts[:scale].to_f
-      shape    = opts[:shape].to_f
-      location = opts[:location].try(:to_f) || 0.0
+      @scale    = opts[:scale].to_f
+      @shape    = opts[:shape].to_f
+      @location = opts[:location].try(:to_f) || 0.0
 
-      if RUBY_PLATFORM == 'java'
-        # create uniform distribution
-        d = UniformRealDistribution.new(@rng, 0.0, 1.0,
-                UniformRealDistribution::DEFAULT_INVERSE_ABSOLUTE_ACCURACY)
-        # setup sampling function
-        @func = Proc.new {
-          # this algorithm was taken from wikipedia
-          u = 1.0 - d.sample
-          location + (scale * ((u ** (-shape)) - 1.0) / shape)
-        }
+      @mean = if @shape < 1.0
+        @location + @scale / (1.0 - @shape)
       else
-        # setup sampling function
-        @func = Proc.new {
-          # this algorithm was taken from wikipedia
-          u = 1.0 - @rng.uniform
-          location + (scale * ((u ** (-shape)) - 1.0) / shape)
-        }
+        Infinity
+      end
+
+      @variance = if @shape < 0.5
+        (@scale ** 2.0) / (((1.0 - @shape) ** 2) * (1.0 - 2 * @shape))
+      else
+        Infinity
       end
     end
+
+    def sample
+      u = 1.0 - @rng.rand
+      if @shape == 0.0
+        @location - @scale * Math::ln(u)
+      else
+        @location + (@scale * ((u ** (- @shape)) - 1.0) / @shape)
+      end
+    end
+
   end
 
 end
---
+++ test/erv/general_pareto_distribution_test.rb
@@ -0,0 +1,82 @@
+require 'test_helper'
+
+require 'erv/general_pareto_distribution'
+
+describe ERV::GpdDistribution do
+
+  NUM_SAMPLES = 50000
+
+  it 'should require scale and shape parameters' do
+    lambda do
+      ERV::GpdDistribution.new()
+    end.must_raise ArgumentError
+  end
+
+
+  context 'a (default) 2-parameter distribution' do
+
+    let :gpd_2params do
+      ERV::GpdDistribution.new(scale: 1.0, shape: 0.3)
+    end
+
+    context 'sampling' do
+
+      it 'should allow sampling' do
+        gpd_2params.sample
+      end
+
+    end
+
+    context 'moments' do
+      let :samples do
+        0.upto(NUM_SAMPLES).collect { gpd_2params.sample }
+      end
+
+      it 'should have the expected mean' do
+        sample_mean = samples.inject(0.0) {|s,x| s += x } / NUM_SAMPLES.to_f
+        sample_mean.must_be_within_epsilon gpd_2params.mean, 0.1
+      end
+
+      it 'should have the expected variance' do
+        sample_variance = samples.inject(0.0) {|s,x| s += (x - gpd_2params.mean) ** 2 } / NUM_SAMPLES.to_f
+        sample_variance.must_be_within_epsilon gpd_2params.variance, 0.1
+      end
+
+    end
+  end
+
+
+  context 'a 3-parameter distribution' do
+
+    let :gpd_3params do
+      ERV::GpdDistribution.new(scale: 1.0,
+                               shape: 0.3,
+                               location: 1.0)
+    end
+
+    context 'sampling' do
+
+      it 'should allow sampling' do
+        gpd_3params.sample
+      end
+
+    end
+
+    context 'moments' do
+      let :samples do
+        0.upto(NUM_SAMPLES).collect { gpd_3params.sample }
+      end
+
+      it 'should have the expected mean' do
+        sample_mean = samples.inject(0.0) {|s,x| s += x } / NUM_SAMPLES.to_f
+        sample_mean.must_be_within_epsilon gpd_3params.mean, 0.1
+      end
+
+      it 'should have the expected variance' do
+        sample_variance = samples.inject(0.0) {|s,x| s += (x - gpd_3params.mean) ** 2 } / NUM_SAMPLES.to_f
+        sample_variance.must_be_within_epsilon gpd_3params.variance, 0.1
+      end
+
+    end
+  end
+end
