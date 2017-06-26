ef4dcfb120266d06cd85ea92b9b0a63ad44ec10e
Mauro Tortonesi <mauro.tortonesi@unife.it>
2017-06-26 17:07:35 +0200

    Platform-agnostic reimplementation of GammaDistribution.

--- lib/erv/gamma_distribution.rb
+++ lib/erv/gamma_distribution.rb
@@ -1,32 +1,65 @@
-if RUBY_PLATFORM == 'java'
-  require 'java'
-  JGammaDistribution = org.apache.commons.math3.distribution.GammaDistribution
-end
-
 require 'erv/distribution'
 
 
 module ERV
-
+  # We refer to the formulation of the Gamma distribution adopted by [KROESE11]:
+  #
+  # f(x) = \frac{\lambda^{\alpha} x^{\alpha - 1} e^{-\lambda x}}{\Gamma(\alpha)}
+  #
+  # with \alpha > 0 being the shape parameter, \lambda > 0 being the scale
+  # parameter, and x > 0.
+  #
+  # (Note that, unlike [KROESE11] Wikipedia refers to the \beta parameter as the "rate".)
   class GammaDistribution < Distribution
+    attr_accessor :mean, :variance
+
     def initialize(opts)
       super(opts)
 
-      raise ArgumentError unless opts[:rate] and opts[:shape]
-      scale = 1 / opts[:rate].to_f
-      shape = opts[:shape].to_f
+      raise ArgumentError unless opts[:scale] and opts[:shape]
+      @scale = opts[:scale].to_f
+      @shape = opts[:shape].to_f
 
-      if RUBY_PLATFORM == 'java'
-        # create distribution
-        d = JGammaDistribution.new(@rng, shape, scale,
-                JGammaDistribution::DEFAULT_INVERSE_ABSOLUTE_ACCURACY)
-        # setup sampling function
-        @func = Proc.new { d.sample }
-      else
-        # setup sampling function
-        @func = Proc.new { @rng.gamma(shape, scale) }
-      end
+      raise ArgumentError, "scale parameter must be greater than zero!" unless @scale > 0.0
+      raise ArgumentError, "shape parameter must be greater than zero!" unless @shape > 0.0
+
+      @mean = @shape / @scale
+      @variance = @shape / (@scale ** 2)
     end
+
+    # We use Marsaglia and Tsang's sampling algorithm
+    #
+    # For more details, see [KROESE11], section 4.2.6, algorithm 4.33 and 
+    # http://www.hongliangjie.com/2012/12/19/how-to-generate-gamma-random-variables/
+    def sample
+      gamrand(@shape, @scale)
+    end
+
+    private
+
+      def gamrand(shape, scale)
+        if shape >= 1.0
+          d = shape - 1.0 / 3
+          c = 1.0 / Math::sqrt(9 * d)
+          ok = false
+          until ok do
+            # Use box-muller algorithm (see [KROESE11], section 4.2.11, algorithm
+            # 4.47) to obtain z ~ N(0,1).
+            u_1 = @rng.rand
+            u_2 = @rng.rand
+            z = Math::sqrt(-2.0 * Math::log(u_1)) * Math::cos(2.0 * Math::PI * u_2)
+            if z > -1.0 / c
+              v = (1.0 + c * z) ** 3
+              u = @rng.rand
+              ok = Math::log(u) <= (0.5 * (z**2) + d - d * v + d * Math::log(v))
+            end
+          end
+          d * v / scale;
+        else # 0 < @shape < 1
+          gamrand(shape + 1.0, scale) * (@rng.rand ** (1 / shape))
+        end
+      end
+
   end
 
 end
---
+++ test/erv/gamma_distribution_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+require 'erv/gamma_distribution'
+
+describe ERV::GammaDistribution do
+
+  NUM_SAMPLES = 20000
+
+  it 'should require the scale and shape parameters' do
+    lambda do
+      ERV::GammaDistribution.new()
+    end.must_raise ArgumentError
+  end
+
+  context 'with a shape greater than one' do
+
+    let :gsgtone do
+      ERV::GammaDistribution.new(shape: 2.0, scale: 0.5)
+    end
+
+    context 'sampling' do
+
+      it 'should allow sampling' do
+        gsgtone.sample
+      end
+
+    end
+
+    context 'moments' do
+      let :samples do
+        0.upto(NUM_SAMPLES).collect { gsgtone.sample }
+      end
+
+      it 'should have the expected mean' do
+        sample_mean = samples.inject(0.0) {|s,x| s += x } / NUM_SAMPLES.to_f
+        sample_mean.must_be_within_epsilon gsgtone.mean, 0.05
+      end
+
+      it 'should have the expected variance' do
+        sample_variance = samples.inject(0.0) {|s,x| s += (x - gsgtone.mean) ** 2 } / NUM_SAMPLES.to_f
+        sample_variance.must_be_within_epsilon gsgtone.variance, 0.05
+      end
+
+    end
+  end
+
+end
