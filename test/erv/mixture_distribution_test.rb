582474f63b57e19a290d9f914be9ac2e6cf87ccf
Mauro Tortonesi <mauro.tortonesi@unife.it>
2017-07-12 17:44:49 +0200

    Fixed and improvements to MixtureDistribution's tests.

--- test/erv/mixture_distribution_test.rb
+++ test/erv/mixture_distribution_test.rb
@@ -3,19 +3,22 @@ require 'test_helper'
 require 'erv/mixture_distribution'
 
 describe ERV::MixtureDistribution do
+
+  let :num_samples { 10000 }
+
   it 'should require at least two distributions' do
     lambda do
       ERV::MixtureDistribution.new([ { distribution: :exponential, rate: 1.0, weight: 0.5 } ])
     end.must_raise ArgumentError
   end
 
-  let :md do
-    ERV::MixtureDistribution.new([ { distribution: :exponential, rate: 1.0, weight: 0.3 },
-                                   { distribution: :exponential, rate: 2.0, weight: 0.2 },
-                                   { distribution: :exponential, rate: 3.0, weight: 0.5 } ])
-  end
-
   context 'sampling' do
+
+    let :md do
+      ERV::MixtureDistribution.new([ { distribution: :exponential, rate: 1.0, weight: 0.3 },
+                                     { distribution: :exponential, rate: 2.0, weight: 0.2 },
+                                     { distribution: :exponential, rate: 3.0, weight: 0.5 } ])
+    end
 
     it 'should allow sampling from the mixture' do
       md.sample
@@ -25,43 +28,24 @@ describe ERV::MixtureDistribution do
 
   context 'moments' do
 
-    context 'with default amplitude' do
-      let :md_expected_mean do
-        0.3 * 1/1.0 + 0.2 * 1/2.0 + 0.5 * 1/3.0
-      end
+    context 'with normalized weights' do
 
-      let :md_expected_variance do
-        0.3 * ((1/1.0 - md_expected_mean) ** 2 + (1/1.0) ** 2) +
-        0.2 * ((1/2.0 - md_expected_mean) ** 2 + (1/2.0) ** 2) +
-        0.5 * ((1/3.0 - md_expected_mean) ** 2 + (1/3.0) ** 2)
-      end
-
-      it 'should correctly calculate the mean of the mixture' do
-        md.mean.must_equal md_expected_mean
-      end
-
-      it 'should correctly calculate the variance of the mixture' do
-        md.variance.must_equal md_expected_variance
-      end
-    end
-
-    context 'with different amplitudes' do
       let :amd do
-        ERV::MixtureDistribution.new([ { distribution: :gaussian, amplitude: 3.0, mean: 1.0, sd: 0.1, weight: 0.3 },
-                                       { distribution: :gaussian, amplitude: 5.0, mean: 2.0, sd: 0.2, weight: 0.2 },
-                                       { distribution: :gaussian, amplitude: 7.0, mean: 3.0, sd: 0.3, weight: 0.5 } ])
+        ERV::MixtureDistribution.new([ { distribution: :gaussian, mean: 1.0, sd: 0.1, weight: 0.3 },
+                                       { distribution: :gaussian, mean: 2.0, sd: 0.2, weight: 0.2 },
+                                       { distribution: :gaussian, mean: 3.0, sd: 0.3, weight: 0.5 } ])
       end
 
       let :amd_expected_mean do
-        0.3 * 3.0 * 1.0 +
-        0.2 * 5.0 * 2.0 +
-        0.5 * 7.0 * 3.0
+        0.3 * 1.0 +
+        0.2 * 2.0 +
+        0.5 * 3.0
       end
 
       let :amd_expected_variance do
-        0.3 * ((3.0 * 1.0 - amd_expected_mean) ** 2 + (3.0 * 0.1) ** 2) +
-        0.2 * ((5.0 * 2.0 - amd_expected_mean) ** 2 + (5.0 * 0.2) ** 2) +
-        0.5 * ((7.0 * 3.0 - amd_expected_mean) ** 2 + (7.0 * 0.3) ** 2)
+        0.3 * ((1.0 - amd_expected_mean) ** 2 + 0.1 ** 2) +
+        0.2 * ((2.0 - amd_expected_mean) ** 2 + 0.2 ** 2) +
+        0.5 * ((3.0 - amd_expected_mean) ** 2 + 0.3 ** 2)
       end
 
       it 'should correctly calculate the mean of the mixture' do
@@ -71,9 +55,25 @@ describe ERV::MixtureDistribution do
       it 'should correctly calculate the variance of the mixture' do
         amd.variance.must_equal amd_expected_variance
       end
+
+      let :samples do
+        0.upto(num_samples).collect { amd.sample }
+      end
+
+      it 'mean of a sample set should match the expected mean' do
+        sample_mean = samples.inject(0.0) {|s,x| s += x } / num_samples.to_f
+        sample_mean.must_be_within_epsilon amd.mean, 0.05
+      end
+
+      it 'variance of a sample set should match the expected variance' do
+        sample_variance = samples.inject(0.0) {|s,x| s += (x - amd.mean) ** 2 } / num_samples.to_f
+        sample_variance.must_be_within_epsilon amd.variance, 0.05
+      end
+
     end
 
     context 'with unnormalized weights' do
+
       let :uwmd do
         ERV::MixtureDistribution.new([ { distribution: :exponential, rate: 1.0, weight: 300 },
                                        { distribution: :exponential, rate: 2.0, weight: 200 },
@@ -96,6 +96,20 @@ describe ERV::MixtureDistribution do
 
       it 'should correctly calculate the variance of the mixture' do
         uwmd.variance.must_equal uwmd_expected_variance
+      end
+
+      let :samples do
+        0.upto(num_samples).collect { uwmd.sample }
+      end
+
+      it 'mean of a sample set should match the expected mean' do
+        sample_mean = samples.inject(0.0) {|s,x| s += x } / num_samples.to_f
+        sample_mean.must_be_within_epsilon uwmd.mean, 0.05
+      end
+
+      it 'variance of a sample set should match the expected variance' do
+        sample_variance = samples.inject(0.0) {|s,x| s += (x - uwmd.mean) ** 2 } / num_samples.to_f
+        sample_variance.must_be_within_epsilon uwmd.variance, 0.05
       end
 
     end
