e81d9fb65c383c1afd3cac487017d52a8ec9ac01
Mauro Tortonesi <mauro.tortonesi@unife.it>
2025-12-30 19:18:59 +0100

    Fixed sampling of discrete uniform distribution.

--- lib/erv/discrete_uniform_distribution.rb
+++ lib/erv/discrete_uniform_distribution.rb
@@ -15,13 +15,12 @@ module ERV
       @min = opts[:min_value].try(:to_i) || 0
       @mean = (@max + @min) / 2.0
       # See https://en.wikipedia.org/wiki/Discrete_uniform_distribution
-      @variance = ((@max - @min + 1) ** 2 - 1) / 12.0
+      @variance = ((@max - @min + 1).to_f ** 2 - 1.0) / 12.0
     end
 
     def sample
-      @min + @rng.rand(@max - @min)
+      (@min.to_f + @rng.rand * (@max - @min + 1).to_f).floor
     end
-
   end
 
 end
